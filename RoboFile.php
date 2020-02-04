f025af9cf8e78bf10f2e27b7b96bb042e0fa3883
Gintautas Miselis <gintautas@miselis.lt>
2020-02-04 18:30:53 +0200

    Fixed build

--- RoboFile.php
+++ RoboFile.php
@@ -7,7 +7,7 @@
 class RoboFile extends \Robo\Tasks
 {
     // define public methods as commands
-    public function prepare()
+    public function prepareDependencies()
     {
         $config = json_decode(file_get_contents(__DIR__ . '/composer.json'), true);
 
@@ -23,8 +23,25 @@ class RoboFile extends \Robo\Tasks
         $config['replace'] = ['codeception/phpunit-wrapper' => '*'];
 
         file_put_contents(__DIR__ . '/composer.json', json_encode($config, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+    }
+
+    public function prepareTests()
+    {
         $this->_copyDir(__DIR__ . '/vendor/codeception/codeception/tests', __DIR__ . '/tests');
         $this->_copy(__DIR__ . '/vendor/codeception/codeception/codeception.yml', __DIR__ .'/codeception.yml');
         $this->_symlink(__DIR__ . '/vendor/bin/codecept', __DIR__ . '/codecept');
+    }
+
+    public function prepareTestAutoloading()
+    {
+        $config = json_decode(file_get_contents(__DIR__ . '/composer.json'), true);
+        $config['autoload-dev'] = [
+            'classmap' => [
+                'tests/cli/_steps',
+                'tests/data/DummyClass.php',
+                'tests/data/claypit/tests/_data'
+            ]
+        ];
+        file_put_contents(__DIR__ . '/composer.json', json_encode($config, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
     }
 }
