ed9d86189fec2480965808a90ba1fd8c96ffb1bf
Gintautas Miselis <gintautas@miselis.lt>
2020-03-29 13:05:46 +0300

    Hide signature differences between different versions of PHPUnit

---
+++ src/Test.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Codeception\PHPUnit;
+
+use PHPUnit\Framework\SelfDescribing;
+use PHPUnit\Framework\Test as PHPUnitTest;
+use PHPUnit\Framework\TestResult;
+
+/**
+ * This class abstracts signature differences between different versions of PHPUnit
+ */
+abstract class Test implements PHPUnitTest, SelfDescribing
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function toString(): string
+    {
+        return $this->_toString();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function run(TestResult $result = null): TestResult
+    {
+        return $this->_run($result);
+    }
+
+    abstract public function _toString();
+
+    abstract public function _run(TestResult $result = null);
+}
