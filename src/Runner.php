3cf9544e857981013bdc27e4c26ff754fb704554
Klaus Heigren <kh@paycheck.dk>
2021-10-19 13:12:52 +0200

    Use stdout stream for reports when available
    Use stdout stream for reports when available. This avoids "Headers
    already sent" types of issues, when manipulating a session during
    testing.

--- src/Runner.php
+++ src/Runner.php
@@ -139,7 +139,10 @@ class Runner extends NonFinal\TestRunner
         }
 
         if ($arguments['report']) {
-            self::$persistentListeners[] = $this->instantiateReporter('report');
+            self::$persistentListeners[] = $this->instantiateReporter(
+                'report',
+                [@fopen(STDOUT, 'w') ?: fopen('php://output', 'w')]
+            );
         }
 
         if ($arguments['html']) {
